1583437f9b0eca9591e16de282ea8115a7602900
neamulbd95 <neamulhaque.naem@gmail.com>
2017-11-28 03:53:07 +0600

    developing First User Story

---
+++ Simple Project Management Tool/Controllers/ItAdminHomeController.cs
@@ -0,0 +1,147 @@
+﻿using Entity;
+using Service_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_Project_Management_Tool.Controllers
+{
+    public class ItAdminHomeController : Controller
+    {
+        //
+        // GET: /ItAdminHome/
+        iUser_Info_Service users = Service_Center.GetUser_Info_Service();
+        iUser_Authentication_Service log = Service_Center.GetUser_Authentication_Service();
+        public ActionResult Index()
+        {
+            if (Convert.ToBoolean(Session["loggedOn"])== true && Convert.ToInt32(Session["userType"])==1)
+            {
+                ViewBag.userTypeName = Session["UserTypeName"];
+                ViewBag.userEmail = Session["UserEmail"];
+                return View();
+            }
+            return View("Index","Error");
+        }
+
+        public ActionResult showUsers()
+        {
+            if (Convert.ToBoolean(Session["loggedOn"]) == true && Convert.ToInt32(Session["userType"]) == 1)
+            {
+                ViewBag.userTypeName = Session["UserTypeName"];
+                ViewBag.userEmail = Session["UserEmail"];
+                return View(users.GetAll());
+            }
+            return View("Index", "Error");
+        }
+
+        public ActionResult addUsers()
+        {
+            if (Convert.ToBoolean(Session["loggedOn"]) == true && Convert.ToInt32(Session["userType"]) == 1)
+            {
+                ViewBag.userTypeName = Session["UserTypeName"];
+                ViewBag.userEmail = Session["UserEmail"];
+                return View();
+            }
+            return View("Index", "Error");
+        }
+
+        public ActionResult AddingUsers()
+        {
+            if (Convert.ToBoolean(Session["loggedOn"]) == true && Convert.ToInt32(Session["userType"]) == 1)
+            {
+                if (Request.Form["AddBtn"] != null)
+                {
+                    try
+                    {
+                        User_Info user = new User_Info();
+                        user.Name = Request.Form["Name"];
+                        user.Email = Request.Form["email"];
+                        user.Status = Request.Form["status"];
+                        //user.Designation = Request.Form["desig"];
+                        if(Convert.ToInt32(Request.Form["desig"])==2)
+                        {
+                            user.Designation = "Project Manager";
+                        }
+                        else if (Convert.ToInt32(Request.Form["desig"]) == 3)
+                        {
+                            user.Designation = "Team Lead";
+                        }
+                        else if (Convert.ToInt32(Request.Form["desig"]) == 4)
+                        {
+                            user.Designation = "Developer";
+                        }
+                        else if (Convert.ToInt32(Request.Form["desig"]) == 5)
+                        {
+                            user.Designation = "QA Engineer";
+                        }
+                        else if (Convert.ToInt32(Request.Form["desig"]) == 6)
+                        {
+                            user.Designation = "UX Engineer";
+                        }
+
+                        User_Authentication userlog = new User_Authentication();
+                        userlog.userEmail = Request.Form["email"];
+                        userlog.password = Request.Form["email"] + "123";
+                        userlog.userType = Convert.ToInt32(Request.Form["desig"]);
+
+                        users.Insert(user);
+                        log.Insert(userlog);
+
+                        return RedirectToAction("showUsers");
+                    }
+                    catch(Exception e)
+                    {
+                        ViewBag.Errormsg = e;
+
+                    }
+                }
+                return View("Index");
+            }
+            return View("Index", "Error");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            if (Convert.ToBoolean(Session["loggedOn"]) == true && Convert.ToInt32(Session["userType"]) == 1)
+            {
+                ViewBag.userTypeName = Session["UserTypeName"];
+                ViewBag.userEmail = Session["UserEmail"];
+                return View(users.GetSingleByID(Id));
+            }
+            return View("Index", "Error");
+            
+        }
+
+        public ActionResult UpdatingUser()
+        {
+            if (Convert.ToBoolean(Session["loggedOn"]) == true && Convert.ToInt32(Session["userType"]) == 1)
+            {
+                if (Request.Form["editBtn"] != null)
+                {
+                    try
+                    {
+                        User_Info user = new User_Info();
+                        user.Id = Convert.ToInt32(Request.Form["id"]);
+                        user.Name = Request.Form["Name"];
+                        user.Email = Request.Form["email"];
+                        user.Status = Request.Form["status"];
+                        user.Designation = Request.Form["desig"];
+
+                        users.Update(user);
+
+                        return RedirectToAction("showUsers");
+                    }
+                    catch (Exception e)
+                    {
+                        ViewBag.Errormsg = e;
+
+                    }
+                }
+            }
+            return View("Index");
+
+        }
+    }
+}
---
+++ Simple Project Management Tool/Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using Entity;
+using Service_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_Project_Management_Tool.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        iUser_Authentication_Service userAuthe = Service_Center.GetUser_Authentication_Service();
+        iUser_Info_Service users = Service_Center.GetUser_Info_Service();
+
+        public ActionResult Index()
+        {
+            ViewBag.ErrorMsg = Session["logInErr"];
+            return View();
+        }
+
+        public ActionResult checkLogin()
+        {          
+            if(Request.Form["submitBtn"] != null)
+            {
+                string userEmail = Request.Form["userEmail"];
+                string password = Request.Form["pass"];
+
+                if (userAuthe.GetSingle(userEmail, password) == null)
+                {
+                    Session["logInErr"] = "Log In Falied. Check UserName Or Passwor";
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    int userType = userAuthe.GetSingle(userEmail, password).userType;
+
+                    if (userType == 1)
+                    {
+                        Session["UserTypeName"] = "IT Admin";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+
+                        return RedirectToAction("Index", "ItAdminHome");
+                    }
+                    else if (userType == 2)
+                    {
+                        Session["UserTypeName"] = "Project Manager";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+                    }
+                    else if (userType == 3)
+                    {
+                        Session["UserTypeName"] = "Team Lead";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+                    }
+                    else if (userType == 4)
+                    {
+                        Session["UserTypeName"] = "Developer";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+                    }
+                    else if (userType == 5)
+                    {
+                        Session["UserTypeName"] = "QA Engineer";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+                    }
+                    else if (userType == 6)
+                    {
+                        Session["UserTypeName"] = "UX Engineer";
+                        Session["userType"] = userType;
+                        Session["UserID"] = users.GetSingleByEmail(userEmail).Id;
+                        Session["UserEmail"] = userEmail;
+                        Session["loggedOn"] = true;
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
